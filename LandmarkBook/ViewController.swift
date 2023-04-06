47f44adc042f6f6781898f45ed6500594b7f2487
Mert can Ünek <mrtcnunk@gmail.com>
2023-04-06 19:42:42 +0300

    update

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
+    
+    
+    
+    
+    
+    // numberOfRowsInSection --> determines the number of cells in any partition. Here we indicate that there are 10 cells in any partition.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // cellForRowAt --> used to determine which data to show for each cell. Here we create a UITableViewCell object for each cell.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        //cell.textLabel?.text = "test"
+        var content = cell.defaultContentConfiguration()
+        content.text = "TableView"
+        content.secondaryText = "Test"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
 
 }
 
